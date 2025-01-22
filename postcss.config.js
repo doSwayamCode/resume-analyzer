ee82d6566cac17feb619a6b4b4164ed4c0bb802e
Swayam Gupta <swayamgupta999@gmail.com>
2025-01-23 00:31:04 +0530

    yo

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+module.exports = {
+  plugins: [
+      require('tailwindcss'), // Adds Tailwind CSS
+      require('autoprefixer') // Adds Autoprefixer
+  ]
 };
